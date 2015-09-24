a198402fa836fb04013edd342d3240d52859b6c6
Phillip Shusta <pshusta@gmail.com>
2015-09-23 20:38:18 -0500

    asdgf

---
+++ Breakout/Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public int yVelocity, xVelocity;
+    public Transform trans;
+
+	// Use this for initialization
+	void Start () {
+        yVelocity = 3;
+        xVelocity = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trans.position = Vector3.Lerp(trans.position, new Vector3(trans.position.x + xVelocity, trans.position.y + yVelocity, trans.position.z), Time.deltaTime);
+	}
+}
--- Breakout/Assets/Tile.cs
+++ Breakout/Assets/Tile.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Tile : MonoBehaviour {
 
     public BoxCollider collider;
+    public GameObject me;
 
     void OnTriggerEnter(Collider _other)
     {
-
+        Debug.Log("Trigger");
+        _other.gameObject.GetComponent<Movement>().yVelocity = _other.gameObject.GetComponent<Movement>().yVelocity * -1;
+        me.SetActive(false);
     }
 
 	// Use this for initialization
