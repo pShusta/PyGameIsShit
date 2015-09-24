cd6c4a7c0e4624e965ebbbf2efacae91f0819e83
Hypno-cube <logan.c.pettit@gmail.com>
2015-09-23 20:42:24 -0500

    move da paddle
    move it good

---
+++ Breakout/Assets/MovePaddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePaddle : MonoBehaviour
+{
+    public float paddleSpeed = 1f;
+
+
+    private Vector3 position = new Vector3(0, -7, 0);
+
+
+    void Update()
+    {
+
+        float X_position = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        position = new Vector3(Mathf.Clamp(X_position, -7.3f, 7.3f), -7f, 0f);
+        transform.position = position;
+
+    }
+}
